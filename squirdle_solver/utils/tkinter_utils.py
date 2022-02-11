f6edaa35d013edd9707a332448e31836e4b6251f
Kevin Lusignolo <Kevin.Lusignolo@hcss.com>
2022-02-11 09:08:14 -0600

    Apply theme

--- squirdle_solver/utils/tkinter_utils.py
+++ squirdle_solver/utils/tkinter_utils.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter.ttk import Widget
 
 def update_widget_text(widget: tk.Widget, value):
     """Takes updates the widget's text to equal whatever Value is specified."""
@@ -22,4 +23,21 @@ def update_widget_text(widget: tk.Widget, value):
             widget.insert("1.0", value)
     elif isinstance(widget, tk.Label) or isinstance(widget, tk.Button):
         widget.config(text=value)
+
+
+
+def set_button_hover_color(button: tk.Widget, hover_color: str):
+    """Sets the background to the hover color when mouse is over btn. Doesn't work for disabled buttons."""
+    original_background = button['bg']
         
+    def on_enter(button: tk.Widget):
+        """Sets the background to the hover color when mouse is over btn. Doesn't work for disabled buttons."""
+        if button['state'] == 'normal':
+            button['bg'] = hover_color
+
+    def on_leave(button: tk.Widget):
+        """Sets the background back to default background when mouse leaves."""
+        button['bg'] = original_background
+    
+    button.bind("<Enter>", lambda _: on_enter(button))
+    button.bind("<Leave>", lambda _: on_leave(button))
--- squirdle_solver/view.py
+++ squirdle_solver/view.py
@@ -1,18 +1,31 @@
 import tkinter as tk
 from tkinter.ttk import Separator
+from turtle import bgcolor
 
 from numpy import var
 from squirdle_solver.models import Pokemon, PokemonGuess, TypeGuessEnum, UpDownEnum
 from squirdle_solver.search_utility import SearchUtility
-from squirdle_solver.utils.tkinter_utils import update_widget_text
+from squirdle_solver.utils.tkinter_utils import set_button_hover_color, update_widget_text
+
+MAIN_BACKGROUND = "#3B4CCA"
+BUTTON_BACKGROUND = "#CC0000"
+BUTTON_BACKGROUND_HOVER = "#FF0000"
+TEXT_BACKGROUND = "#7FCCEC"
+TEXT_FOREGROUND = "#F7F6F2"
+TEXT_ENTRY_FOREGROUND = "#070808"
+SEPARATOR_BACKGROUND = "#FFDE00"
 
 class UpDownCorrectRadioFrame(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master=master)
+        self.configure(bg=MAIN_BACKGROUND)
         self.radio_var = tk.IntVar()
-        self.up_radio = tk.Radiobutton(self, text="Up", value=UpDownEnum.Up.value, variable=self.radio_var)
-        self.down_radio = tk.Radiobutton(self, text="Down", value=UpDownEnum.Down.value, variable=self.radio_var)
-        self.correct_radio = tk.Radiobutton(self, text="Yes", value=UpDownEnum.Correct.value, variable=self.radio_var)
+        self.up_radio = tk.Radiobutton(self, text="Up", value=UpDownEnum.Up.value, variable=self.radio_var,
+            bg=MAIN_BACKGROUND, fg=TEXT_ENTRY_FOREGROUND, selectcolor=TEXT_BACKGROUND, activebackground=MAIN_BACKGROUND)
+        self.down_radio = tk.Radiobutton(self, text="Down", value=UpDownEnum.Down.value, variable=self.radio_var,
+            bg=MAIN_BACKGROUND, fg=TEXT_ENTRY_FOREGROUND, selectcolor=TEXT_BACKGROUND, activebackground=MAIN_BACKGROUND)
+        self.correct_radio = tk.Radiobutton(self, text="Yes", value=UpDownEnum.Correct.value, variable=self.radio_var,
+            bg=MAIN_BACKGROUND, fg=TEXT_ENTRY_FOREGROUND, selectcolor=TEXT_BACKGROUND, activebackground=MAIN_BACKGROUND)
 
         self.up_radio.grid(row=0, column=0)
         self.down_radio.grid(row=0, column=1)
@@ -26,10 +39,14 @@ class UpDownCorrectRadioFrame(tk.Frame):
 class TypeRadioFrame(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master=master)
+        self.configure(bg=MAIN_BACKGROUND)
         self.radio_var = tk.IntVar()
-        self.incorrect_radio = tk.Radiobutton(self, text="No", value=TypeGuessEnum.Incorrect.value, variable=self.radio_var)
-        self.out_of_radio = tk.Radiobutton(self, text="Out of Pos.", value=TypeGuessEnum.WrongPosition.value, variable=self.radio_var)
-        self.correct_radio = tk.Radiobutton(self, text="Yes", value=TypeGuessEnum.Correct.value, variable=self.radio_var)
+        self.incorrect_radio = tk.Radiobutton(self, text="No", value=TypeGuessEnum.Incorrect.value, variable=self.radio_var,
+            bg=MAIN_BACKGROUND, fg=TEXT_ENTRY_FOREGROUND, selectcolor=TEXT_BACKGROUND, activebackground=MAIN_BACKGROUND)
+        self.out_of_radio = tk.Radiobutton(self, text="Out of Pos.", value=TypeGuessEnum.WrongPosition.value, variable=self.radio_var,
+            bg=MAIN_BACKGROUND, fg=TEXT_ENTRY_FOREGROUND, selectcolor=TEXT_BACKGROUND, activebackground=MAIN_BACKGROUND)
+        self.correct_radio = tk.Radiobutton(self, text="Yes", value=TypeGuessEnum.Correct.value, variable=self.radio_var,
+            bg=MAIN_BACKGROUND, fg=TEXT_ENTRY_FOREGROUND, selectcolor=TEXT_BACKGROUND, activebackground=MAIN_BACKGROUND)
 
         self.incorrect_radio.grid(row=0, column=0)
         self.out_of_radio.grid(row=0, column=1)
@@ -44,15 +61,16 @@ class PokemonGuessFrame(tk.Frame):
     pokemon: Pokemon = None
     def __init__(self, master=None):
         tk.Frame.__init__(self, master=master)
+        self.configure(bg=MAIN_BACKGROUND)
 
-        pokemon_name_lbl = tk.Label(self, text="Pokemon:")
-        generation_lbl = tk.Label(self, text="Generation:")
-        type_one_lbl = tk.Label(self, text="Type One:")
-        type_two_lbl = tk.Label(self, text="Type Two:")
-        height_lbl = tk.Label(self, text="Height:")
-        weight_lbl = tk.Label(self, text="Weight:")
+        pokemon_name_lbl = tk.Label(self, text="Pokemon:", bg=MAIN_BACKGROUND, fg=TEXT_FOREGROUND)
+        generation_lbl = tk.Label(self, text="Generation:", bg=MAIN_BACKGROUND, fg=TEXT_FOREGROUND)
+        type_one_lbl = tk.Label(self, text="Type One:", bg=MAIN_BACKGROUND, fg=TEXT_FOREGROUND)
+        type_two_lbl = tk.Label(self, text="Type Two:", bg=MAIN_BACKGROUND, fg=TEXT_FOREGROUND)
+        height_lbl = tk.Label(self, text="Height:", bg=MAIN_BACKGROUND, fg=TEXT_FOREGROUND)
+        weight_lbl = tk.Label(self, text="Weight:", bg=MAIN_BACKGROUND, fg=TEXT_FOREGROUND)
 
-        self.pokemon_name_entry = tk.Entry(self, width=20)
+        self.pokemon_name_entry = tk.Entry(self, width=20, bg=TEXT_BACKGROUND, fg=TEXT_ENTRY_FOREGROUND, insertbackground=TEXT_ENTRY_FOREGROUND)
         self.generation_radios = UpDownCorrectRadioFrame(self)
         self.type_one_radios = TypeRadioFrame(self)
         self.type_two_radios = TypeRadioFrame(self)
@@ -66,16 +84,16 @@ class PokemonGuessFrame(tk.Frame):
         height_lbl.grid(column=8, row=0)
         weight_lbl.grid(column=10, row=0)
 
-        self.pokemon_name_entry.grid(column=0, row=1)
-        Separator(self, orient=tk.VERTICAL).grid(row=0, column=1, rowspan=2, sticky="ns", padx=2)
+        self.pokemon_name_entry.grid(column=0, row=1, padx=3)
+        tk.Frame(self, width=1, bd=0, bg=SEPARATOR_BACKGROUND).grid(row=0, column=1, rowspan=2, sticky="ns", padx=2)
         self.generation_radios.grid(column=2, row=1)
-        Separator(self, orient=tk.VERTICAL).grid(row=0, column=3, rowspan=2, sticky="ns", padx=2)
+        tk.Frame(self, width=1, bd=0, bg=SEPARATOR_BACKGROUND).grid(row=0, column=3, rowspan=2, sticky="ns", padx=2)
         self.type_one_radios.grid(column=4, row=1)
-        Separator(self, orient=tk.VERTICAL).grid(row=0, column=5, rowspan=2, sticky="ns", padx=2)
+        tk.Frame(self, width=1, bd=0, bg=SEPARATOR_BACKGROUND).grid(row=0, column=5, rowspan=2, sticky="ns", padx=2)
         self.type_two_radios.grid(column=6, row=1)
-        Separator(self, orient=tk.VERTICAL).grid(row=0, column=7, rowspan=2, sticky="ns", padx=2)
+        tk.Frame(self, width=1, bd=0, bg=SEPARATOR_BACKGROUND).grid(row=0, column=7, rowspan=2, sticky="ns", padx=2)
         self.height_radios.grid(column=8, row=1)
-        Separator(self, orient=tk.VERTICAL).grid(row=0, column=9, rowspan=2, sticky="ns", padx=2)
+        tk.Frame(self, width=1, bd=0, bg=SEPARATOR_BACKGROUND).grid(row=0, column=9, rowspan=2, sticky="ns", padx=2)
         self.weight_radios.grid(column=10, row=1)
 
     def error_text(self) -> str:
@@ -101,8 +119,10 @@ class MainFrame(tk.Frame):
     pokemon_guess_frames: "list[PokemonGuessFrame]"
     def __init__(self, master):
         tk.Frame.__init__(self, master=master)
-        self.scrollbar = tk.Scrollbar(self)
-        self.output_text_widget = tk.Text(self, state='disabled', yscrollcommand=self.scrollbar.set, height=10)
+        self.configure(bg=MAIN_BACKGROUND)
+        self.scrollbar = tk.Scrollbar(self, bg=TEXT_BACKGROUND, troughcolor=MAIN_BACKGROUND)
+        self.output_text_widget = tk.Text(self, state='disabled', yscrollcommand=self.scrollbar.set, height=10,
+            bg=TEXT_BACKGROUND, fg=TEXT_ENTRY_FOREGROUND, insertbackground=TEXT_ENTRY_FOREGROUND, bd=2, wrap=tk.WORD)
         self.scrollbar.config(command=self.output_text_widget.yview)
 
         pokemon_guess_1 = PokemonGuessFrame(self)
@@ -123,27 +143,30 @@ class MainFrame(tk.Frame):
         ]
 
         self.search_btn = tk.Button(self, width=35, text="Search", command=self.search)
+        self.search_btn.configure(bg=BUTTON_BACKGROUND, fg=TEXT_FOREGROUND, activebackground=BUTTON_BACKGROUND_HOVER,
+                       activeforeground=TEXT_FOREGROUND, bd=2, relief='groove')
+        set_button_hover_color(self.search_btn, BUTTON_BACKGROUND_HOVER)
 
         self.output_text_widget.grid(row=0, padx=5, pady=5, sticky="ew")
         self.scrollbar.grid(row=0, column=1, sticky="ns", pady=5)
 
-        Separator(self, orient="horizontal").grid(row=1, sticky="ew", padx=2)
+        tk.Frame(self, height=1, bd=0, bg=SEPARATOR_BACKGROUND).grid(row=1, sticky="ew", padx=2)
 
         pokemon_guess_1.grid(row=2, padx=5, pady=2)
-        Separator(self, orient="horizontal").grid(row=3, sticky="ew", padx=2)
+        tk.Frame(self, height=1, bd=0, bg=SEPARATOR_BACKGROUND).grid(row=3, sticky="ew", padx=2)
         pokemon_guess_2.grid(row=4, pady=2)
-        Separator(self, orient="horizontal").grid(row=5, sticky="ew", padx=2)
+        tk.Frame(self, height=1, bd=0, bg=SEPARATOR_BACKGROUND).grid(row=5, sticky="ew", padx=2)
         pokemon_guess_3.grid(row=6, pady=2)
-        Separator(self, orient="horizontal").grid(row=7, sticky="ew", padx=2)
+        tk.Frame(self, height=1, bd=0, bg=SEPARATOR_BACKGROUND).grid(row=7, sticky="ew", padx=2)
         pokemon_guess_4.grid(row=8, pady=2)
-        Separator(self, orient="horizontal").grid(row=9, sticky="ew", padx=2)
+        tk.Frame(self, height=1, bd=0, bg=SEPARATOR_BACKGROUND).grid(row=9, sticky="ew", padx=2)
         pokemon_guess_5.grid(row=10, pady=2)
-        Separator(self, orient="horizontal").grid(row=11, sticky="ew", padx=2)
+        tk.Frame(self, height=1, bd=0, bg=SEPARATOR_BACKGROUND).grid(row=11, sticky="ew", padx=2)
         pokemon_guess_6.grid(row=12, pady=2)
-        Separator(self, orient="horizontal").grid(row=13, sticky="ew", padx=2)
+        tk.Frame(self, height=1, bd=0, bg=SEPARATOR_BACKGROUND).grid(row=13, sticky="ew", padx=2)
         pokemon_guess_7.grid(row=14, pady=2)
 
-        Separator(self, orient="horizontal").grid(row=15, sticky="ew", padx=2)
+        tk.Frame(self, height=1, bd=0, bg=SEPARATOR_BACKGROUND).grid(row=15, sticky="ew", padx=2)
 
         self.search_btn.grid(row=16, pady=3)
         
